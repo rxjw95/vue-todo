1ed42fd42fae683116bb723295865335888e5743
Jangwook Ryu <rxjw95@gmail.com>
2021-09-20 18:44:05 +0900

    Add original array for keep the original data

--- src/components/TodoTemplate.vue
+++ src/components/TodoTemplate.vue
@@ -46,6 +46,11 @@ export default {
   },
   data() {
     return {
+      original: [
+        {id: 0, details: "스프링 공부", status: "active"},
+        {id: 1, details: "스프링 중", status: "done"},
+        {id: 2, details: "스프링 포기", status: "active"},
+      ],
       todos: [
         {id: 0, details: "스프링 공부", status: "active"},
         {id: 1, details: "스프링 중", status: "done"},
@@ -59,6 +64,7 @@ export default {
   methods: {
     onClickAllCheckBtn(state) {
        this.todos = this.todos.map(todo => { return {...todo, status: state ? 'active' : 'done'}});
+       this.original = [...this.todos];
     },
     onChangeEdit(details) {
       this.editDetails = details;
@@ -69,11 +75,13 @@ export default {
         return;
       }
       this.todos = [...this.todos, {id: this.todos.length, details: this.editDetails, status: 'active'}];
+      this.original = [...this.todos];
       this.editDetails = "";
     },
 
     onClickCheckBtn(id) {
       this.todos = this.todos.map(todo => todo.id === id ? { ...todo, status: todo.status === 'active' ? 'done' : 'active' } : todo);
+      this.original = [...this.todos];
       this.reflectMode(this.filterMode);
     },
     onChangeDetails(details) {
@@ -83,29 +91,33 @@ export default {
       this.changeDetails = this.changeDetails.trim();
       if(!this.changeDetails) {
         this.todos = this.todos.filter(todo => todo.id !== id);
+        this.original = [...this.todos];
         return;
       }
       this.todos = this.todos.map(todo => todo.id === id ? { ...todo, details: this.changeDetails} : todo)
+      this.original = [...this.todos];
       this.editDetails = "";
     },
     onClickRemoveBtn(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+      this.original = [...this.todos];
     },
 
     onClickFilterAllBtn(mode) {
       this.filterMode = mode;
-      this.todos = this.todos.filter(todo => todo); 
+      this.todos = this.original.filter(todo => todo); 
     },
     onClickFilterActiveBtn(mode) {
       this.filterMode = mode;
-      this.todos = this.todos.filter(todo => todo.status === 'active');
+      this.todos = this.original.filter(todo => todo.status === 'active');
     },
     onClickFilterCompletedBtn(mode) {
       this.filterMode = mode;
-      this.todos = this.todos.filter(todo => todo.status === 'done');
+      this.todos = this.original.filter(todo => todo.status === 'done');
     },
     onClickClearBtn(){
       this.todos = this.todos.filter(todo => todo.status === "active");
+      this.original = [...this.todos];
     },
     
     reflectMode(filter) {
